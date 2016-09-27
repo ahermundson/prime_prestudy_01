c9dfa6ddc0be98a0d1ffee79cb6447ceed982260
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-09-27 08:04:38 -0500

    Add comments to script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+/*
+In the instructions it stated to create a function that consoles "Hey this works" to the console.
+I wasn't sure if we were just supposed to put a console.log message here or create and call a function
+which does it. 
+*/
+
 function confirmation() {
-console.log("Hey this works");
+  console.log("Hey this works");
 }
 
 confirmation();
